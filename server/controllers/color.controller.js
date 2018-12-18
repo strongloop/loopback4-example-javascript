98cbe8edd7107e37b6697cc4f084b1366c763b0a
Hage Yaapa <hage.yaapa@in.ibm.com>
2018-12-18 22:54:19 +0530

    clean-up

--- server/controllers/color.controller.js
+++ server/controllers/color.controller.js
@@ -1,3 +1,7 @@
+// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 "use strict";
 const helpers = require('../../lib/helpers.js');
 const decorate = helpers.decorate;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,7 @@
+// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 "use strict";
 
 exports.Color = require('./color.model').Color;
