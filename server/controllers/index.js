cd7e7bd8181c1cdca048fe8ed8c46787eb7e8766
Hage Yaapa <hage.yaapa@in.ibm.com>
2019-01-09 14:08:28 +0530

    reset

--- server/controllers/index.js
+++
@@ -1,8 +0,0 @@
-// Copyright IBM Corp. 2017,2018. All Rights Reserved.
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-"use strict";
-
-exports.PinController = require('./ping.controller');
-exports.ColorController = require('./color.controller');
