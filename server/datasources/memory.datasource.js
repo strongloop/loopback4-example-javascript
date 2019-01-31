b2a2532a160c49a45e4d487b70c0c63a716cebd5
Hage Yaapa <hage.yaapa@in.ibm.com>
2019-01-31 14:37:10 +0530

    datasource generator

--- server/datasources/memory.datasource.js
+++ server/datasources/memory.datasource.js
@@ -1,21 +1,5 @@
 'use strict';
 
-const { decorate, metadata, param } = require('../../lib/helpers.js');
-const core = require("@loopback/core");
-const repository = require("@loopback/repository");
-const config = require("./memory.datasource.json");
-
-class MemoryDataSource extends repository.juggler.DataSource {
-  constructor(dsConfig = config) {
-    super(dsConfig);
-  }
-};
-
-MemoryDataSource.dataSourceName = 'memory';
-
-MemoryDataSource = decorate([
-  param(0, core.inject('datasources.config.memory', { optional: true })),
-  metadata("design:paramtypes", [Object])
-], MemoryDataSource);
-
-exports.MemoryDataSource = MemoryDataSource;
+const config = require('./memory.datasource.json');
+const generateDataSource = require('../../lib/datasource-generator.js');
+exports.MemoryDataSource = generateDataSource('memory', config);
